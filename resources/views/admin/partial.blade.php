07af3b3fcd5171e64a7c6b6fc52cb0a44707e404
NguyenQuynh1994 <nguyenquynhkqh@gmail.com>
2016-05-26 16:00:50 +0700

    Admin: management Category

---
+++ resources/views/admin/partial.blade.php
@@ -0,0 +1,9 @@
+<div class="col-lg-3">
+    <div class="thumbnail">
+        <div class="list-group">
+            <a href={{ url('/admin/category') }} class="list-group-item">Category</a>
+            <a href={{ url('/admin/word') }} class="list-group-item">Word</a>
+            <a href={{ url('/admin/user') }} class="list-group-item">User</a>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,9 +37,10 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
                     @if (Auth::check() && Auth::user()->isAdmin())
-                        <li><a href="{{ url('/admin/user') }}">Admin</a></li>
+                        <li><a href="{{ url('/admin/category') }}">Admin</a></li>
+                    @else
+                        <li><a href="{{ url('/home') }}">Home</a></li>
                     @endif
                 </ul>
                 <!-- Right Side Of Navbar -->
